e73b897d6d5bcc89438a1730e6120347220874e6
Simon Kofod <github@kofod.it>
2017-03-13 23:47:05 +0100

    Added initial constructors

---
+++ RomanNumeral.Tests/RomanNumeralTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+
+namespace RomanNumeral.Tests
+{
+    public class RomanNumeralTest
+    {
+        [Fact]
+        public void IntegerConstructor_IsNotImplemented()
+        {
+            Assert.Throws<NotImplementedException>(() => new RomanNumeral(0));
+        }
+
+        [Fact]
+        public void StringConstructor_IsNotImplemented()
+        {
+            Assert.Throws<NotImplementedException>(() => new RomanNumeral(""));
+        }
+    }
+}
---
+++ RomanNumeral/RomanNumeral.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RomanNumeral
+{
+    public class RomanNumeral
+    {
+        public RomanNumeral(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RomanNumeral(string value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
